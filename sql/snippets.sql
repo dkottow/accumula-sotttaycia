3dc1136fe65c11e708f8f4aff5b6f875f5d7d322
Daniel Kottow <dkottow@gmail.com>
2021-06-29 18:20:55 -0400

    el orden de las columnas de los user table types debe ser alfabetico

--- sql/snippets.sql
+++ sql/snippets.sql
@@ -1,1 +1,20 @@
-EXEC sp_rename '[BigQuery].[Servel].CreatedQuery', 'TerrID', 'COLUMN';
+--EXEC sp_rename '[BigQuery].[Servel].CreatedQuery', 'TerrID', 'COLUMN';
+select *  from VerificaYa.Correos order by CreatedOn desc
+
+--select *  from Smartcob.Pagos order by CreatedOn desc
+--delete  from Smartcob.Pagos where CreatedFromProcess = 'Test'
+
+--select *  from Smartcob.Receptores order by CreatedOn desc
+
+--select *  from Smartcob.CPH_PT order by CreatedOn desc
+
+-- select *  from Smartcob.Mensajes order by CreatedOn desc
+
+-- drop TYPE ut_Sinacofi_BienesRaices
+--select * from Sinacofi.BienesRaices
+
+--select * from Sinacofi.Vehiculos
+
+--select * from Sinacofi.Direcciones
+
+--alter table BigQuery.Servel add CreatedByUser VARCHAR(80);
